1e164155132ad01224e988b66cb4e27d31740351
buzzon <buzzondev@gmail.com>
2020-10-09 23:31:04 +0500

    fix

--- plushkin/core/models.py
+++ plushkin/core/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-
-class Medicine(models.Model):
-    title = models.CharField(max_length=256)
-
-
-class Substances(models.Model):
-    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
-    doza = models.CharField(max_length=256)
