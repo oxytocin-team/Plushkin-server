84f4190507aa34498a0d5538df45d66c734b28c1
buzzon <buzzondev@gmail.com>
2020-10-18 13:23:17 +0500

    bookmark permissions
    all auth users can read
    and only owner can edit

--- plushkin/bookmark/api/urls.py
+++ plushkin/bookmark/api/urls.py
@@ -5,5 +5,6 @@ from .views import BookmarkViewSet
 app_name = 'bookmark'
 urlpatterns = [
     url(r'bookmarks/$', BookmarkViewSet.as_view({'get': 'list', 'post': 'create'}), name='account-list'),
-    url(r'bookmarks/(?P<pk>[^/]+)$', BookmarkViewSet.as_view({'get': 'retrieve', 'post': 'update'}), name='account-retrieve'),
+    url(r'bookmarks/(?P<pk>[^/]+)$', BookmarkViewSet.as_view({'get': 'retrieve', 'post': 'update'}),
+        name='account-retrieve'),
 ]
--- plushkin/bookmark/api/views.py
+++ plushkin/bookmark/api/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
+from bookmark.api.permissions import IsOwnerOrReadOnly
 from bookmark.api.serializers import BookmarkSerializer
 from bookmark.models import Bookmark
 
@@ -7,5 +8,5 @@ from bookmark.models import Bookmark
 class BookmarkViewSet(viewsets.ModelViewSet):
     serializer_class = BookmarkSerializer
     queryset = Bookmark.objects.all()
-
-
+    permission_classes = [permissions.IsAuthenticated,
+                          IsOwnerOrReadOnly]
